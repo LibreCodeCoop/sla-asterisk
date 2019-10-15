551df2fec69974c0e94088d0f23e2a34dfaff7e9
Vitor Mattos <vitor@php.rio>
2019-10-14 22:44:25 -0300

    Exibindo os dados corretos para a métrica buscada

--- public/update.php
+++ public/update.php
@@ -5,19 +5,23 @@ require_once '../bootstrap.php';
 $sth = $conn->prepare(
     <<<QUERY
 SELECT TIME_FORMAT(created, "%H:%i") AS date,
-       sla
+       history.sla
   FROM history
- WHERE queue = 610
-   AND created >= '2019-10-14 18:35:30'
+  JOIN config
+    ON config.queue = history.queue
+   AND config.metric_id = history.metric_id
+  JOIN metric
+    ON metric.id = history.metric_id
+ WHERE config.queue = ?
+   AND history.created >= DATE_SUB(NOW(), INTERVAL config.window second)
+   AND metric.name = ?
 QUERY
 );
-$sth->execute();
+$sth->execute([$_GET['queue'], $_GET['type']]);
 while ($row = $sth->fetch(\PDO::FETCH_ASSOC)) {
     $data['labels'][] = $row['date'];
     $data['data'][] = $row['sla'];
 }
-
-$sth->execute([1,2,3]);
 
 header('Content-Type: application/json');
 echo json_encode($data);
