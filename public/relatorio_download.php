7b9e00ba9004f571eabbcb70663d8f635a8fc90b
LiviaGouvea <gouvea.lc@gmail.com>
2019-11-05 13:10:00 -0300

    Inclusão de datimepicker para escolha de datas nos relatórios

--- public/relatorio_download.php
+++ public/relatorio_download.php
@@ -7,19 +7,27 @@ require_once '../bootstrap.php';
 
 if ($_POST['relatorio_historico']) {
     
-    $sql = "SELECT id, queue, sla, metric_id, created FROM dashboard.history
+    $sql = "SELECT h.id, h.queue, c.descr, h.sla, h.metric_id, m.name, h.created 
+
+            FROM dashboard.history h
+            
+            JOIN asterisk.queues_config c ON h.queue = c.extension
+            
+            JOIN  dashboard.metric m ON h.metric_id = m.id
+
             WHERE queue in (".$_POST['queue'].") and metric_id in (".$_POST['metric_id'].")
+
             and created >= '".$_POST['data_inicio']."' and created <= '".$_POST['data_fim']."' ";    
     
     $sth = $conn->prepare($sql);
     $sth->execute();
 
-    $user_CSV[0] = array('queue', 'sla', 'metrica', 'data_criacao');
+    $user_CSV[0] = array('Fila', 'SLA', 'Métrica', 'Data criacão');
     
     $i = 1;
     while ($row = $sth->fetch(\PDO::FETCH_ASSOC)) {    
         
-        $user_CSV[$i] = array($row['queue'],$row['sla'],$row['metric_id'],$row['created']);
+        $user_CSV[$i] = array($row['queue']." - ".$row['descr'],$row['sla'],$row['name'],$row['created']);
         
         $i++;
     } 
