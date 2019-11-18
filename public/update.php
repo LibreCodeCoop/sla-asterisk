07376f1d6dad064d9b44310cf4d3547195cd0aca
Vitor Mattos <vitor@php.rio>
2019-11-18 00:18:54 -0300

    Remoção de código sem uso Redução do intervalo de 24 para 1h no gráfico de linha de Fila

--- public/update.php
+++ public/update.php
@@ -2,53 +2,11 @@
 
 require_once '../bootstrap.php';
 
-if ($_GET['type'] == 'ura') {
-    $data = new StdClass();
-    $data->datasets = [];
-    $data->datasets[0] = new StdClass();
-    $data->datasets[0]->backgroundColor = [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-    ];
-    $data->datasets[0]->borderColor = [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-    ];
-    $data->datasets[0]->borderWidth = 1;
-    $data->datasets[0]->label =  'URA';
-    $conn->query('USE asteriskcdrdb;');
-    $sth = $conn->prepare(
-        <<<QUERY
-        SELECT id.name,
-               COUNT(*) AS total
-          FROM cdr
-          JOIN asterisk.ivr_details id
-            ON SUBSTRING(dcontext, 5) = id.id
-         WHERE dcontext LIKE 'ivr-%'
-           AND id.name NOT LIKE '%Pesquisa%'
-           AND cdr.calldate > DATE_SUB(NOW(), INTERVAL 24 HOUR)
-         GROUP BY id.name,
-                  dcontext
-        QUERY
-        );
-    $sth->execute();
-    while ($row = $sth->fetch(\PDO::FETCH_ASSOC)) {
-        $data->labels[] = $row['name'];
-        $data->datasets[0]->data[] = $row['total'];
-    }
-    header('Content-Type: application/json');
-    echo json_encode($data);
-    return;
+if ($_GET['type'] == 'Fila') {
+    $interval = 1;
+} else {
+    $interval = 24;
 }
-
 $sth = $conn->prepare(
     <<<QUERY
 SELECT TIME_FORMAT(created, "%H:%i") AS date,
@@ -60,7 +18,7 @@ SELECT TIME_FORMAT(created, "%H:%i") AS date,
   JOIN metric
     ON metric.id = history.metric_id
  WHERE config.queue = ?
-   AND history.created >= DATE_SUB(NOW(), INTERVAL 24 HOUR)
+   AND history.created >= DATE_SUB(NOW(), INTERVAL {$interval} HOUR)
    AND metric.name = ?
  ORDER BY metric.order, created
 QUERY
